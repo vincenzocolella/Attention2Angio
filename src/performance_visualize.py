804b3e3561935749ab230dd63c0bc069c3238d37
vincenzocolella <vincenzo.colella@hotmail.com>
2022-03-07 11:36:08 +0100

    Update performance_visualize.py

--- src/performance_visualize.py
+++ src/performance_visualize.py
@@ -212,7 +212,7 @@ def summarize_performance_global(step, g_model,d_model, dataset, n_samples=3,sav
         pyplot.axis('off')
         pyplot.imshow(X_realA_half[i])
         try:
-          pyplot.imsave('/content/drive/Mydrive/Attention2Angio/Results/real_source/{}_{}.png'.format(step+1,i+1),X_realA_half[i])
+          pyplot.imsave('/content/Attention2Angio/Results/real_source/{}_{}.png'.format(step+1,i+1),X_realA_half[i])
         except Exception:
           pass
     # plot generated target image
@@ -220,14 +220,14 @@ def summarize_performance_global(step, g_model,d_model, dataset, n_samples=3,sav
         pyplot.subplot(3, n_samples, 1 + n_samples + i)
         pyplot.axis('off')
         generated = X_fakeB_half[:,:,:,0]
-        pyplot.imsave('/content/drive/Mydrive/Attention2Angio/Results/fake/{}_{}.png'.format(step+1,i+1),generated[i],cmap='gray')
+        pyplot.imsave('/content/Attention2Angio/Results/fake/{}_{}.png'.format(step+1,i+1),generated[i],cmap='gray')
         pyplot.imshow(generated[i],cmap="gray")
     # plot real target image
     for i in range(n_samples):
         pyplot.subplot(3, n_samples, 1 + n_samples*2 + i)
         pyplot.axis('off')
         twoD_img = X_realB_half[:,:,:,0]
-        pyplot.imsave('/content/drive/Mydrive/Attention2Angio/Results/real_target/{}_{}.png'.format(step+1,i+1),twoD_img[i],cmap='gray')
+        pyplot.imsave('/content/Attention2Angio/Results/real_target/{}_{}.png'.format(step+1,i+1),twoD_img[i],cmap='gray')
         pyplot.imshow(twoD_img[i],cmap="gray")
     # save plot to file
     filename1 = savedir+'/global_plot_%06d.png' % (step+1)
@@ -243,21 +243,21 @@ def summarize_performance_global(step, g_model,d_model, dataset, n_samples=3,sav
 
     # SSIM
 
-    ssim_1 = ssim(generated[1], twoD_img[1], data_range=generated[1].max() - generated[1].min())
+    ssim_1 = ssim(generated[0], twoD_img[0], data_range=generated[1].max() - generated[1].min())
     print("SSIM between first target and result:")
     print(ssim_1)
 
-    ssim_2 = ssim(generated[2], twoD_img[2], data_range=generated[2].max() - generated[2].min())
+    ssim_2 = ssim(generated[1], twoD_img[1], data_range=generated[2].max() - generated[2].min())
     print("SSIM between second target and result:")
     print(ssim_2)
 
-    ssim_3 = ssim(generated[3], twoD_img[3], data_range=generated[3].max() - generated[3].min())
+    ssim_3 = ssim(generated[2], twoD_img[2], data_range=generated[3].max() - generated[3].min())
     print("SSIM between third target and result:")
     print(ssim_3)
 
-    ssim_4 = ssim(generated[4], twoD_img[4], data_range=generated[4].max() - generated[4].min())
+    ssim_4 = ssim(generated[3], twoD_img[3], data_range=generated[4].max() - generated[4].min())
     print("SSIM between fourth target and result:")
-    print(ssim_3)
+    print(ssim_4)
 
     
 
