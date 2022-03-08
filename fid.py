c9fd83171dc37b4b15a4d85f8e55297b7baed5f6
vincenzocolella <vincenzo.colella@hotmail.com>
2022-03-08 11:42:25 +0100

    Update fid.py

--- fid.py
+++ fid.py
@@ -1,11 +1,21 @@
 
 from cleanfid import fid
+
 #FID
-score_FID = fid.compute_fid('/content/Attention2Angio/Results/real_target','/content/Attention2Angio/Results/fake',mode="legacy_tensorflow")
-   
-print(score_FID)
+
+def compute_fid():
+    score_FID = fid.compute_fid('/content/Attention2Angio/Results/real_target','/content/Attention2Angio/Results/fake',mode="legacy_tensorflow")
+    
+    return score_FID
 
 #KID
-score_KID = fid.compute_kid('/content/Attention2Angio/Results/real_target','/content/Attention2Angio/Results/fake',mode="legacy_tensorflow")
+
+def compute_kid():
+    score_KID = fid.compute_kid('/content/Attention2Angio/Results/real_target','/content/Attention2Angio/Results/fake',mode="legacy_tensorflow")
+        
+    return score_KID
     
-print(score_KID)
+    
+if __name__ == '__main__':
+    print(compute_fid())
+    print(compute_kid())
